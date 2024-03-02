0e8ef5678cca82ed31dbdd1bcbe4da9109524e02
Rampa0 <willjgass@gmail.com>
2024-03-01 16:17:31 -0800

    changes to schemas - need review

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 122aProject
-122a DB project
+# MySQL 
+- --
+make sure you have MySQL installed and are able to run a server locally as this is how we will be able to test and run our code.
+
+Im using pycharm professional to work on this project because it has useful integrated DB tools that will help with the development process in this project
+
+Clone this repository and you will have access to the modules ive outlined so far. 
+
+## TODO
+- --
+- MySQL schemas 
+- Function Requirements (1-12)
+- unittesting
--- src/schema.sql
+++ src/schema.sql
@@ -1,144 +1,74 @@
-USE main;
-
---User table
-CREATE TABLE Users (
-    UCINetID VARCHAR(20) PRIMARY KEY NOT NULL,
-    FirstName VARCHAR(50),
-    MiddleName VARCHAR(50),
-    LastName VARCHAR(50)
-);
-CREATE TABLE UserEmail (
-  UCINetID VARCHAR(20)  NOT NULL,
-  Email VARCHAR(1024),
-  PRIMARY KEY (UCINetID, Email)
-  FOREIGN KEY (UCINetID) REFERENCES Users (UCINetID)
-    ON DELETE CASCADE
+-- User Table
+CREATE TABLE User (
+    UCINetID VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255),
+    name_first VARCHAR(255),
+    name_middle VARCHAR(255),
+    name_last VARCHAR(255)
 );
 
-
--- Student Delta Table
-CREATE TABLE Students (
-    UCINetID VARCHAR(20) PRIMARY KEY NOT NULL,
-    FOREIGN KEY (UCINetID) REFERENCES Users(UCINetID)
-      ON DELETE CASCADE
+-- Student Table
+CREATE TABLE Student (
+    UCINetID VARCHAR(255) PRIMARY KEY,
+    FOREIGN KEY (UCINetID) REFERENCES User(UCINetID) ON DELETE CASCADE
 );
 
-
--- Administrator Delta Table
-CREATE TABLE Administrators (
-    UCINetID VARCHAR(20) NOT NULL,
-    FOREIGN KEY (UCINetID) REFERENCES Users(UCINetID)
-      ON DELETE CASCADE
+-- Administrator Table
+CREATE TABLE Administrator (
+    UCINetID VARCHAR(255) PRIMARY KEY,
+    FOREIGN KEY (UCINetID) REFERENCES User(UCINetID) ON DELETE CASCADE
 );
-CREATE TABLE AdministratorsShiftTiming (
-    UCINetID VARCHAR(20) PRIMARY KEY NOT NULL,
-    ShiftTiming VARCHAR(50),
-    PRIMARY KEY (UCINetID, ShiftTiming)
-    FOREIGN KEY (UCINetID) REFERENCES Administrators (UCINetID)
-    ON DELETE CASCADE
-);
-
 
 -- Course Table
-CREATE TABLE Courses (
-    CourseID INT PRIMARY KEY NOT NULL,
-    Title VARCHAR(100),
-    Quarter VARCHAR(20)
+CREATE TABLE Course (
+    course_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    quarter VARCHAR(255)
 );
-
 
 -- Project Table
-CREATE TABLE Projects (
-    ProjectID INT PRIMARY KEY NOT NULL,
-    Name VARCHAR(100),
-    Description TEXT,
-    CourseID INT NOT NULL,
-    FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+CREATE TABLE Project (
+    project_id INT PRIMARY KEY,
+    description TEXT,
+    name VARCHAR(255)
 );
-
 
 -- Machine Table
-CREATE TABLE Machines (
-    MachineID INT PRIMARY KEY NOT NULL,
-    IPAddress VARCHAR(15),
-    OperationalStatus VARCHAR(50),
-    Location VARCHAR(255)
-);
-CREATE TABLE MachineHostnames (
-    MachineID INT NOT NULL,
-    Hostname VARCHAR(255),
-    PRIMARY KEY (MachineID, Hostname)
-    FOREIGN KEY (MachineID) REFERENCES Machines (MachineID)
-    ON DELETE CASCADE
+CREATE TABLE Machine (
+    machine_id INT PRIMARY KEY,
+    hostname VARCHAR(255),
+    IP_address VARCHAR(255),
+    operational_status VARCHAR(255),
+    location VARCHAR(255)
 );
 
-
--- Software Table
-CREATE TABLE Software (
-    SoftwareID INT PRIMARY KEY NOT NULL,
-    Name VARCHAR(100),
-    VersionNumber VARCHAR(50),
-    LicenseKey VARCHAR(255)
+-- Use Table
+CREATE TABLE UseRecord (
+    start_date DATE,
+    end_date DATE,
+    UCINetID VARCHAR(255),
+    project_id INT,
+    machine_id INT,
+    PRIMARY KEY (start_date, end_date, UCINetID, project_id, machine_id),
+    FOREIGN KEY (UCINetID) REFERENCES User(UCINetID),
+    FOREIGN KEY (project_id) REFERENCES Project(project_id),
+    FOREIGN KEY (machine_id) REFERENCES Machine(machine_id)
 );
 
-
--- Task Table
-CREATE TABLE Tasks (
-    TaskID INT PRIMARY KEY NOT NULL,
-    MachineID INT NOT NULL,
-    Description TEXT,
-    PriorityLevel INT,
-    AssignedDate  DATETIME,
-    BeginTime DATETIME,
-    EndTime DATETIME,
-    FOREIGN KEY (MachineID) REFERENCES Machines (MachineID)
+-- Have (Course-Project Relationship) Table
+CREATE TABLE Have (
+    course_id INT,
+    project_id INT,
+    PRIMARY KEY (course_id, project_id),
+    FOREIGN KEY (course_id) REFERENCES Course(course_id),
+    FOREIGN KEY (project_id) REFERENCES Project(project_id)
 );
 
-
--- MaintenanceRecord Table
-CREATE TABLE MaintenanceRecords (
-    RecordID INT PRIMARY KEY NOT NULL,
-    ServiceDate DATE,
-    Duration INT,
-    ServiceType VARCHAR(100),
-    MachineID INT NOT NULL,
-    FOREIGN KEY (MachineID) REFERENCES Machines(MachineID)
+-- Manage (Machine-Administrator Relationship) Table
+CREATE TABLE Manage (
+    machine_id INT,
+    UCINetID VARCHAR(255),
+    PRIMARY KEY (machine_id, UCINetID),
+    FOREIGN KEY (machine_id) REFERENCES Machine(machine_id),
+    FOREIGN KEY (UCINetID) REFERENCES Administrator(UCINetID)
 );
-
-
--- Install Relationship Table
-CREATE TABLE SoftwareOfProjectInstallOnMachine (
-    MachineID INT,
-    SoftwareID INT,
-    ProjectID INT,
-    InstallDate DATE,
-    PRIMARY KEY (MachineID, SoftwareID, ProjectID),
-    FOREIGN KEY (MachineID) REFERENCES Machines(MachineID),
-    FOREIGN KEY (SoftwareID) REFERENCES Software(SoftwareID),
-    FOREIGN KEY (ProjectID) REFERENCES Software(ProjectID)
-);
-
-
--- Use Relationship Table
-CREATE TABLE StudentUseMachinesInProject (
-    ProjectID INT,
-    StudentUCINetID VARCHAR(20),
-    MachineID INT,
-    StartDate DATE,
-    EndDate DATE,
-    PRIMARY KEY (ProjectID, StudentUCINetID, MachineID),
-    FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID),
-    FOREIGN KEY (StudentUCINetID) REFERENCES Students(UCINetID),
-    FOREIGN KEY (MachineID) REFERENCES Machines(MachineID)
-);
-
-
--- Administrator Machine Management Table
-CREATE TABLE AdministratorManageMachines (
-    AdministratorUCINetID VARCHAR(20),
-    MachineID INT,
-    PRIMARY KEY (AdministratorUCINetID, MachineID),
-    FOREIGN KEY (AdministratorUCINetID) REFERENCES Administrators(UCINetID),
-    FOREIGN KEY (MachineID) REFERENCES Machines(MachineID)
-);
-
